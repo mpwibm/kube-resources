5619065f14040123d9cefec9cef48bd40bd2da0f
mpw07458 <mpw10128@gmail.com>
2019-07-16 08:41:41 -0400

    add initial openshift scripts

---
+++ script/openshift/get_openshift.sh
@@ -0,0 +1,6 @@
+oc get projects --all_namespaces
+oc get deploy --all_namespaces
+oc get nodes -o wide
+oc get pods --all-namespaces
+oc get secrets --all-namespaces
+oc get ingress --all-namespaces
